bb9f21c600ef45bd8faee173b5d7b880fc864b41
julien <pernin.julien@gmail.com>
2026-05-10 14:18:03 +0200

    Navbar added

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+"use client";
+
+import React from "react";
 
 const PropertyPage = () => {
-  return (
-    <div>PropertyPage</div>
-  )
-}
+  return <div>PropertyPage</div>;
+};
 
-export default PropertyPage
+export default PropertyPage;
