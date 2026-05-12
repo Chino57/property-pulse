1f890b2ab234003ba9536528a92965a06f5a740a
julien <pernin.julien@gmail.com>
2026-05-12 15:15:25 +0200

    HomeProperties added on the Homepage

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import './globals.css';
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
+
 export const metadata = {
     title: 'PropertyPulse | Find The Perfect Rental',
     description: 'Find your dream rental property',
