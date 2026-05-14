bf0ef3585d5ca3a09a10d667e10bf36cd59bbd39
julien <pernin.julien@gmail.com>
2026-05-14 22:27:27 +0200

    Individual property pages added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import './globals.css';
+import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-
 export const metadata = {
-    title: 'PropertyPulse | Find The Perfect Rental',
-    description: 'Find your dream rental property',
-    keywords: 'rental, find rentals, find properties'
-}
+  title: "PropertyPulse | Find The Perfect Rental",
+  description: "Find your dream rental property",
+  keywords: "rental, find rentals, find properties",
+};
+
 
 const MainLayout = ({ children }) => {
   return (
