e0265f8c5c29bfc21e3aab43086daeae085efa5c
julien <pernin.julien@gmail.com>
2026-05-25 08:11:58 +0200

    Bookmark button request

--- app/api/bookmarks/route.js
+++ app/api/bookmarks/route.js
@@ -9,16 +9,16 @@ export const POST = async (request) => {
   try {
     await connectDB();
     const { propertyId } = await request.json();
-    const sessionUser = getSessionUser();
+    const sessionUser = await getSessionUser();
 
-    if (!session || !session.userId) {
+    if (!sessionUser || !sessionUser.userId) {
       return new Response("User ID is required", { status: 401 });
     }
 
     const { userId } = sessionUser;
 
     // Find user in database
-    const user = await User.findOne({ _id: user });
+    const user = await User.findOne({ _id: userId });
 
     // Check if property is bookmarked
     let isBookmarked = user.bookmarks.includes(propertyId);
